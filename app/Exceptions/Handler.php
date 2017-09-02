2a23d11c3c644d35a9ceba6834126ac6fd027888
Het Shah <htshah60@gmail.com>
2017-09-02 23:15:34 +0530

    Explicit JSON output for api
    All api urls and urls with header "accept:application/json" will have json as response in case of exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,10 +9,10 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
+    * A list of the exception types that should not be reported.
+    *
+    * @var array
+    */
     protected $dontReport = [
         \Illuminate\Auth\AuthenticationException::class,
         \Illuminate\Auth\Access\AuthorizationException::class,
@@ -21,46 +21,74 @@ class Handler extends ExceptionHandler
         \Illuminate\Session\TokenMismatchException::class,
         \Illuminate\Validation\ValidationException::class,
     ];
-
+    
     /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
+    * Report or log an exception.
+    *
+    * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+    *
+    * @param  \Exception  $exception
+    * @return void
+    */
     public function report(Exception $exception)
     {
         parent::report($exception);
     }
-
+    
     /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        //return response()->view('errors.403');
-        return parent::render($request, $exception);
+    * Render an exception into an HTTP response.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Exception  $exception
+    * @return \Illuminate\Http\Response
+    */
+    public function render($request, Exception $e)
+    {   // If the request wants JSON (AJAX doesn't always want JSON)
+        $url = url()->current();
+        if ($request->wantsJson() || str_contains($url,'api')) {
+            // Define the response
+            $response = [
+                'errors' => 'Sorry, something went wrong.'
+            ];
+            
+            // If the app is in debug mode
+            if (config('app.debug')) {
+                // Add the exception class name, message and stack trace to response
+                $response['exception'] = get_class($e); // Reflection might be better here
+                $response['message'] = $e->getMessage();
+                $response['trace'] = $e->getTrace();
+            }
+            
+            // Default response of 400
+            $status = 400;
+            
+            // If this exception is an instance of HttpException
+            if ($this->isHttpException($e)) {
+                // Grab the HTTP status code from the Exception
+                $status = $e->getStatusCode();
+            }
+            
+            // Return a JSON response with the response array and status code
+            return response()->json($response, $status);
+        }
+        
+        // Default to the parent class' implementation of handler
+        return parent::render($request, $e);
     }
-
+    
     /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Response
-     */
+    * Convert an authentication exception into an unauthenticated response.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Illuminate\Auth\AuthenticationException  $exception
+    * @return \Illuminate\Http\Response
+    */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
         if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
-
+        
         return redirect()->guest(route('login'));
     }
 }
