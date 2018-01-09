8c6b6fe66410899b2a3a8841af2c083354f1e936
Het Shah <htshah60@gmail.com>
2018-01-09 19:46:33 +0530

    Fixes in SubjectController

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 use App\Department;
 use App\ClassRoom;
 use App\MapClassSubject;
@@ -20,17 +22,35 @@ class SubjectController extends Controller
 	 */
 	public function getClassSubjects(Request $request)
     {
-		// Retrieve department ID
-    	$department = Department::where('name','=',$request->department)->first();
-
-		// Retrieve classroom ID
-    	$class = ClassRoom::where(['year'=>$request->year,
-    		'department_id'=>$department->id,
-    		'division'=>$request->division])->first();
-
+		try{
+			// Retrieve department ID
+			$department = Department::where('name','=',$request->department)->firstOrFail();
+		}catch(ModelNotFoundException $e){
+			return $this->sendFailedResponse("Given classroom was not found.");
+		}
+		try{
+			// Retrieve classroom ID
+			$class = ClassRoom::where(['year'=>$request->year,
+				'department_id'=>$department->id,
+				'division'=>$request->division])->firstOrFail();
+		}catch(ModelNotFoundException $e){
+			return $this->sendFailedResponse("Given classroom was not found.");
+		}
 		// Retrieve all associated subject ID
     	$subjectList = MapClassSubject::where('class_id','=',$class->id)->pluck('subject_id');
 		// Return all associated subject details
 		return response()->json(['success'=>1,'subjects'=>Subject::find($subjectList)]);
-    }
+	}
+	
+	/**
+	 * Returns a failed reponse with an
+	 * error message
+	 *
+	 * @param string $error
+	 * @return void
+	 */
+	public function sendFailedResponse(string $error)
+	{
+		return response()->json(['error'=>$error]);
+	}
 }
