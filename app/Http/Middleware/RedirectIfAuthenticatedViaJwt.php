bc1140e3df4f476aa68e30fb1678220d32c15780
Het Shah <htshah60@gmail.com>
2018-01-10 21:24:12 +0530

    Added new routes. Fixes to login and logout

--- app/Http/Middleware/RedirectIfAuthenticatedViaJwt.php
+++ app/Http/Middleware/RedirectIfAuthenticatedViaJwt.php
@@ -5,33 +5,50 @@ namespace App\Http\Middleware;
 use Closure;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class RedirectIfAuthenticatedViaJwt
 {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
+    * Handle an incoming request.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Closure  $next
+    * @param  string|null  $guard
+    * @return mixed
+    */
     public function handle($request, Closure $next, $guard = null)
     {
         try{
-            if (JWTAuth::parseToken()->authenticate()) {
-                $token = JWTAuth::getToken();
-                if (($request->isMethod('get') && $request->is(route('login'))) || 
-                !$request->expectsJson()) {
-                    return redirect()->route('home',['token'=>$token]);
+            try{
+                if (JWTAuth::parseToken()->authenticate()) {
+                    $token = JWTAuth::getToken();
+                    if (($request->isMethod('get') && $request->is(route('login'))) || 
+                    !$request->expectsJson()){
+                        return redirect()->route('home',['token'=>$token]);
+                    }
+                    
+                    //Return error in json format, if json is expected
+                    return response()->json(['error'=>'Access denied.','redirect'=>'/?token='.$token]);
                 }
-            
-                return response()->json(['error'=>'Access denied.','redirect'=>'/?token='.$token]);
+            }catch(\Exception $e){
+                // If there is token error, then only show
+                // the error if request is except for login.
+                if(!$request->is('*/login')){
+                    // Throw error if url is other than
+                    // login
+                    throw $e;
+                }
             }
-        }catch(JWTException $e){
-            // return response()->json(['error'=>$e]);
+        }catch (TokenExpiredException $e) {
+            return response()->json(['error'=>'token_expired']);
+        }catch (TokenInvalidException $e) {
+            return response()->json(['error'=>'token_invalid']);
+        }catch (JWTException $e) {
+            return response()->json(['error'=>'token_absent']);
         }
-
+        
         return $next($request);
     }
 }
